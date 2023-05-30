0ea12de69c379cbc0646ce97ea8706c48a9bc499
ChenEylon <cheneylon1@gmail.com>
2023-05-30 15:43:44 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Myevents from './components/myevents'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <Myevents/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/myevents.jsx
@@ -0,0 +1,15 @@
+
+import './myevents.css'
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+
+const Myevents = () => {
+    const [value, onChange] = useState(new Date());
+  return (
+    <div>
+        <Calendar onChange={onChange} value={value} />
+        </div>
+  )
+}
+
+export default Myevents
