c8720a4cc0a995753ac20f7838ad9e30d7e9ef83
ChenEylon <cheneylon1@gmail.com>
2023-05-31 16:45:02 +0300

    eventpage create

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import DataContext from '../DataContext';
 import "./EventPage.css"
 export const EventPage = () => {
+  const theContext = useContext(DataContext);
+  const theData = theContext
+  const { id } = useParams();
+  const dataid = theData?.find((view) =>view.id == id);
+
   return (
-    <div>EventPage</div>
+    <div className='eventpage'>
+      <div className="imgdiv"   >
+        <img className='img' src={dataid?.event_image} alt="nothing" />
+      </div>
+
+      <div id='main-section'>
+        <div className='headline'>
+          <h1 className='lecture'>{dataid?.event_name}</h1>
+        </div>
+        <div className='rest'>
+          <div className='firstrest'>
+
+            <h4 className='time'>  {dataid?.date}  </h4>
+            <div>
+              <span className='name'>  {dataid?.lecturer_name} </span>
+              <span className='price' id='lecturerinfo'>{dataid?.lecturer_info} </span>
+            </div>
+            <h3 className='price'>How much:85</h3>
+            <div className='third'>
+              <h3>
+                date and time:  {dataid?.start_time} P.M, {dataid?.date}
+              </h3>
+              <h3>
+                Location:  "{dataid?.place_name}", {dataid?.street_addres}, {dataid?.city}
+              </h3>
+            </div>
+          </div>
+          <div className='secondrest'>
+            <h5 className='place'>this is a map</h5>
+            <h5 className='place'>this is the capacity</h5>
+            <h5 className='place'>for contact:{dataid?.email} </h5>
+            <button>this is the buy</button>
+          </div>
+
+
+
+
+        </div>
+        <h6 className='headlinebottom'> The MindGame of the day:</h6>
+        <h6 className='description'> {theData[0].event_info}</h6>
+      </div>
+    </div>
+
   )
 }
