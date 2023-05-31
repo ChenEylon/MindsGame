28a5b6904b9541a0a5fd87c9d3ac2d2afd0fb1ec
SpeedyQ1 <sahar.y.ash@gmail.com>
2023-05-31 21:29:51 +0300

    chaning cards structure2

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -18,27 +18,32 @@ export const EventCard = ({ index }) => {
   let city = theData[index].city;
   let id = theData[index].id;
 
+  if(price==0){
+    price = "free";
+  }
+  else{
+    price = `${price}$`;
+  }
 
 
   return (
     <div id="card-container">
       <div id="image-container">
         <img id="the-img" src={imgUrl} />
-      </div>
-      <div id="info">
-        <h3>"{title}"</h3>
+        <div id="info">
         <div id="card-discription">
+        <h3>"{title}"</h3>
           <span>
             {date} , {startAt}
           </span>
           <span> <LocationOnIcon/> "{placeName}", {city}</span>
         </div>
+      </div>
+      </div>
         <div id="buy-section">
-        <span id="price">{price}$</span>
+        <span id="price">{price}</span>
         <Link className="link" to={`/Events/${id}`}><span id="view-btn">view</span></Link>
         </div>
-        
-      </div>
     </div>
   );
 };
