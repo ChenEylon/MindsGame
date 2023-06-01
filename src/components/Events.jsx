e485a70f8217931e9cb4fddbec78aca0dbf9cfac
SpeedyQ1 <sahar.y.ash@gmail.com>
2023-06-01 17:58:53 +0300

    convert to local storage from arr

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,15 +1,14 @@
-import React, { useContext } from 'react';
-import DataContext from '../DataContext';
+import React, { useState } from 'react';
 import "./Events.css"
 import { EventCard } from './EventCard';
+
 export const Events = () => {
-  const theContext = useContext(DataContext);
-  const theData = theContext[0]
-  const setTheData = theContext[1]
+  const [dataArr, setDataArr] = useState(JSON.parse(localStorage.getItem("dataArr")));
+
   return (
     <div>
       <div id='event-container'>
-        {theData.map((value,index)=><EventCard key={index} index={index} />)}
+        {dataArr.map((value,index)=><EventCard key={index} index={index} />)}
       </div>
     </div>
   )
