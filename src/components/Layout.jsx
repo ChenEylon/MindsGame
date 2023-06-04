8e14bd49878b4529e9725a821a0738c46b484e34
SpeedyQ1 <sahar.y.ash@gmail.com>
2023-06-04 12:29:23 +0300

    create sign in and signup

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,82 +1,92 @@
-import React from 'react'
-import "./Layout.css"
-import { Link, Outlet } from 'react-router-dom'
+import "./Layout.css";
+import { Link, Outlet } from 'react-router-dom';
 import { CDBModalFooter, CDBBox, CDBBtn, CDBIcon } from 'cdbreact';
+import React, { useEffect, useState } from 'react';
 
+export const Layout = ({isLogIn,setIsLogIn}) => {
+  const [loginNav, setLoginNav] = useState("");
+  useEffect(() => {
+    setLoginNav(loginState())
+  }, [isLogIn]);
 
-export const Layout = () => {
+  function loginState() {
+    if (!isLogIn) {
+      return <Link to="/Login" className="link navLink">Login</Link>
+    }
+    else{
+      return <button id="logoutBtn" className="navLink" onClick={()=>{localStorage.removeItem('loginData'); setIsLogIn(false)}}>Log out</button>
+    }
+    
+  }
   return (
-    <div id='body-container' >
+    <div id='body-container'>
       <nav id='navbar'>
         <Link to='/' className="link"><img className="logo" src="../../logo-with-background.png" alt="MindGames Logo" /></Link>
         <Link to='/' className="link navLink">Home</Link>
         <Link to='/Events' className="link navLink">Events</Link>
-        <Link to='/Myevents' className="link navLink">My Events </Link>
-        <Link to="/Login" className="link navLink">Login</Link>
+        <Link to='/Myevents' className="link navLink">My Events</Link>
+        {loginNav}
+        
       </nav>
       <div id="under-nav">
-        
         <div id='outlet'>
-          <Outlet/>
-        </div> 
-      
-      
-     
-      <CDBModalFooter className="shadow" style={{backgroundColor: '#E0E1DD'}}>
-        <CDBBox display="flex" flex="column" className="mx-auto " style={{ width: '90%' ,paddingBottom :"1.5rem", paddingTop :"1.5rem" }}>
-          <CDBBox display="flex" justifyContent="between" className="flex-wrap">
-            <CDBBox>
-              <a href="/" className="d-flex align-items-center p-0 text-dark">
-              <img className="logo" src="../../logo-with-background.png" alt="MindGames Logo" />
-              </a>
-              <p className="my-3" style={{ width: '250px' }}>
-                MindGames: Unleashing the power of knowledge through captivating lectures and seamless ticketing.
-              </p>
-              <CDBBox display="flex" className="mt-4">
-                <CDBBtn flat color="dark">
-                  <CDBIcon fab icon="facebook-f" />
-                </CDBBtn>
-                <CDBBtn flat color="dark" className="mx-3">
-                  <CDBIcon fab icon="twitter" />
-                </CDBBtn>
-                <CDBBtn flat color="dark" className="p-2">
-                  <CDBIcon fab icon="linkedin-in" />
-                </CDBBtn>
+          <Outlet />
+        </div>
 
+        <CDBModalFooter className="shadow" style={{ backgroundColor: '#E0E1DD' }}>
+          <CDBBox display="flex" flex="column" className="mx-auto " style={{ width: '90%', paddingBottom: "1.5rem", paddingTop: "1.5rem" }}>
+            <CDBBox display="flex" justifyContent="between" className="flex-wrap">
+              <CDBBox>
+                <a href="/" className="d-flex align-items-center p-0 text-dark">
+                  <img className="logo" src="../../logo-with-background.png" alt="MindGames Logo" />
+                </a>
+                <p className="my-3" style={{ width: '250px' }}>
+                  MindGames: Unleashing the power of knowledge through captivating lectures and seamless ticketing.
+                </p>
+                <CDBBox display="flex" className="mt-4">
+                  <CDBBtn flat color="dark">
+                    <CDBIcon fab icon="facebook-f" />
+                  </CDBBtn>
+                  <CDBBtn flat color="dark" className="mx-3">
+                    <CDBIcon fab icon="twitter" />
+                  </CDBBtn>
+                  <CDBBtn flat color="dark" className="p-2">
+                    <CDBIcon fab icon="linkedin-in" />
+                  </CDBBtn>
+                </CDBBox>
+              </CDBBox>
+              <CDBBox>
+                <p className="h5 mb-4" style={{ fontWeight: '600' }}>
+                  MindGames
+                </p>
+                <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0', display: 'flex' }}>
+                  <Link to='/'>About Us</Link>
+                  <Link to='/Contactus'>Contact</Link>
+                </CDBBox>
+              </CDBBox>
+              <CDBBox>
+                <p className="h5 mb-4" style={{ fontWeight: '600' }}>
+                  Help
+                </p>
+                <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0', display: 'flex' }}>
+                  <Link to='/Login'>Sign In</Link>
+                </CDBBox>
+              </CDBBox>
+              <CDBBox>
+                <p className="h5 mb-4" style={{ fontWeight: '600' }}>
+                  Products
+                </p>
+                <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0', display: 'flex' }}>
+                  <Link>Windframe</Link>
+                  <Link>Loop</Link>
+                  <Link>Contrast</Link>
+                </CDBBox>
               </CDBBox>
             </CDBBox>
-            <CDBBox>
-              <p className="h5 mb-4" style={{ fontWeight: '600' }}>
-                MindGames
-              </p>
-              <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0', display: 'flex' }}>
-                <Link> About Us</Link>
-                <Link to='/Contactus'> Contact</Link>
-              </CDBBox>
-            </CDBBox>
-            <CDBBox>
-              <p className="h5 mb-4" style={{ fontWeight: '600' }}>
-                Help
-              </p>
-              <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0', display: 'flex' }}>
-                <Link>Sign In</Link>
-              </CDBBox>
-            </CDBBox>
-            <CDBBox>
-              <p className="h5 mb-4" style={{ fontWeight: '600' }}>
-                Products
-              </p>
-              <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0', display: 'flex' }}>
-                <Link>Windframe</Link>
-                <Link>Loop</Link>
-                <Link>Contrast</Link>
-              </CDBBox>
-            </CDBBox>
+            <small className="text-center">&copy; MindGames Ltd</small>
           </CDBBox>
-          <small className="text-center" >&copy; MindGames Ltd</small>
-        </CDBBox>
-      </CDBModalFooter>
+        </CDBModalFooter>
       </div>
     </div>
-  )
-}
+  );
+};
